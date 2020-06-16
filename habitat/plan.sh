58e32890019c1926de5193d539786e097bf79afa
Yonathan Koren <yonkornilov@live.com>
2020-06-16 05:45:28 -0400

    Get rid of optional scaffolding_go_base_path variable.

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -5,7 +5,6 @@ pkg_name=golang-habitat-hello-world
 pkg_origin=shuttleops-golang-example
 pkg_scaffolding="core/scaffolding-go"
 pkg_bin_dirs=(bin)
-scaffolding_go_base_path="github.com/yonkornilov"
 
 do_check() {
     go test main.go main_test.go
