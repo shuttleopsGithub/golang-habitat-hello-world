943181e03f0930000a44d476fbefe50ccdbeae10
Yonathan Koren <yonkornilov@live.com>
2020-06-17 11:21:27 -0400

    Remove testing logic to further simplify example package.

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -5,7 +5,3 @@ pkg_name=golang-habitat-hello-world
 pkg_origin=shuttleops-golang-example
 pkg_scaffolding="core/scaffolding-go"
 pkg_bin_dirs=(bin)
-
-do_check() {
-    go test main.go main_test.go
-}
