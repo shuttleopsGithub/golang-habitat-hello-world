f49711cfe451a95b5cf9b05da88381962f52f95d
Yonathan Koren <yonkornilov@live.com>
2020-06-19 10:16:07 -0400

    Add health-check hooks which checks if Hello World has been printed out to stderr in the last [threshold] seconds.

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
 	for {
-		log.Println(message())
+		log.Println(message(os.Args[1]))
 		time.Sleep(time.Second)
 	}
 }
 
-func message() string {
-	return "Hello World"
+func message(message string) string {
+	return message
 }
